2020a54764d32aebebe9911e214a041330e23e62
sblick19 <sophiecblick@gmail.com>
2024-02-02 22:27:23 -0600

    Added the print statements with the needed information.

---
+++ src/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name: Sophie Blick, Github ID: sblick19");
+		System.out.println("Name: Andi Petersen, Github ID: Antedbell20");
+		System.out.println("Name: DJ Hernandez, Github ID: djhkbw");
+		System.out.println("Name: Corbin Sheridan, Github ID: CorbinSheridan");
+	}
+
+}
